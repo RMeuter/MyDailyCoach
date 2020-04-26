8227fdc4923f290103e12e2dd239fba0f077d8b3
Romain Meuter <r.meuter@hotmail.com>
2020-04-26 17:05:47 +0200

    Néttoyage fichier

--- src/main.js
+++ src/main.js
@@ -1,37 +1,47 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './routes';
-import * as firebase from "firebase";
 import store from "./store";
 
+// ########### Firebase #############
+import * as firebase from "firebase";
 import VueFirestore from 'vue-firestore';
 
+// ########### Bootstrap #############
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+
 import "@/helpers/filters";
+
+
+Vue.config.productionTip = false
+
+
+// ############## firebase ##############
+const config = {
+  apiKey: "AIzaSyD6czB7vsdnpAFW_NOS6tGyNGkzawNGGZI",
+  authDomain: "happyevening.firebaseapp.com",
+  databaseURL: "https://happyevening.firebaseio.com",
+  projectId: "happyevening",
+  storageBucket: "happyevening.appspot.com",
+  messagingSenderId: "776464730949",
+  appId: "1:776464730949:web:28ccf2bd57a7afa5350c2c",
+  measurementId: "G-KM3FPZMGT5"
+};
+
+firebase.initializeApp(config);
+export const db = firebase.firestore();
 
 firebase.auth().onAuthStateChanged(user => {
   store.dispatch("fetchUser", user);
 });
 
-Vue.config.productionTip = false
-//Vue.config.silent = true
-
-/*
-// handle page reloads
-let app
-firebaseconfig.auth.onAuthStateChanged(user => {
-  console.log(user)
-  if (!app) {
-    app = new Vue({
-      el: '#app',
-      
-      render: h => h(App)
-    })
-  }
-})
-*/
-
+// ############## Pluggin ##############
+Vue.use(BootstrapVue);
 Vue.use(VueFirestore);
 
+// ############## Vue ##############
 new Vue({
   router,
   store,
