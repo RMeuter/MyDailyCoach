db50b36eba6befb791bf3b49191cde765e9c8dac
Romain Meuter <r.meuter@hotmail.com>
2020-05-01 17:19:30 +0200

    Ajout de déconnection, parametre, et du parametre user

--- src/components/StatsUser.vue
+++ src/components/StatsUser.vue
@@ -1,8 +1,44 @@
 <template>
   <div>
-
     <b-container >
-      <h4 class="alert-heading text-center mb-5">Bienvenue Bob</h4>
+      <b-row>
+        <b-col cols="4">
+          <h4 class="alert-heading text-center mb-5">Bienvenue {{user}} </h4>
+        </b-col>
+        <b-col offset="1" cols="3">
+          <b-button v-b-toggle.collapse-1 variant="white">Paramètre</b-button>
+        </b-col>
+        <b-col cols="4">
+          <b-button variant="danger" @click.prevent="signOut">Se déconnecter</b-button>
+        </b-col>
+          <b-collapse id="collapse-1" class="mt-2 col-12">
+            <b-card class="row text-center">
+              <b-col class="mb-4">
+                <p class="card-text">Quel horraire de recommandation voulez vous ?</p>
+                <b-button v-b-toggle.collapse-1-inner size="sm">Modifiez</b-button>
+                <b-collapse id="collapse-1-inner" class="mt-2">
+                  <b-card>
+                    https://mdbootstrap.com/docs/jquery/forms/time-picker/
+                  </b-card>
+                </b-collapse>
+              </b-col>
+              <b-col  class="mb-4">
+                <p class="card-text">Quel capteur voulez vous ?</p>
+                <b-button v-b-toggle.collapse-2-inner size="sm">Modifiez</b-button>
+                <b-collapse id="collapse-2-inner" class="mt-2">
+                  <b-card>Hello!</b-card>
+                </b-collapse>
+              </b-col>
+              <b-col  class="mb-4">
+                <p class="card-text">Quel capteur voulez vous ?</p>
+                <b-button v-b-toggle.collapse-3-inner size="sm">Modifiez</b-button>
+                <b-collapse id="collapse-3-inner" class="mt-2">
+                  <b-card>Hello!</b-card>
+                </b-collapse>
+              </b-col>
+            </b-card>
+          </b-collapse>
+      </b-row>
       <b-row v-if="firstTime"> 
         <b-alert show variant="light" dismissible>
           <p>Voici comment fonctionne l'application :</p>
@@ -26,7 +62,7 @@
         </b-col>
       </b-row>          
     </b-container>
-    <div v-if="outHourButtonShow">
+    <div ><!-- v-if="outHourButtonShow">!-->
       <b-link href="/signin">Accès à la recommandation de la journée</b-link>
     </div>
   </div>
@@ -35,26 +71,40 @@
 <script>
 import Chart from "./Charts/Chart";
 import Mixed from "./Charts/Mixed";
+import { mapGetters } from "vuex";
+import * as firebase from "firebase";
 
 export default {
-  name: "MyTest",
-
+  name: "StatsUser",
   components:{
     Chart,
     Mixed
   },
   methods:{
-    firstTime(){
+    signOut() {
+       firebase
+       .auth()
+       .signOut()
+       .then(() =>
+        this.$router.replace({name : "signin"})
+       )
+    },
+    firstTime(){ // ça doit passer en props !
       return true
     }
   },
   computed: {
-    outHourButtonShow(){
-      let date = new Date();
-      return date.getHours()>=0 && date.getHours()<=2;
-    },
-  }
+      outHourButtonShow(){
+        let date = new Date();
+        return date.getHours()>=0 && date.getHours()<=2;
+    }
+  },
+  ...mapGetters({
+      user:"user",
+  })
+  
 };
+
 </script>
 
 <style scoped>
@@ -72,8 +122,5 @@ button.nav-link.signout {
   text-align: center;
   border-radius: 5px;
   border: none;
-}
-.display-name {
-  margin-right: 2em;
 }
 </style>
