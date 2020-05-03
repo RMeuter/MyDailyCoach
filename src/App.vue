33ae059d339ce48756092e062518f2b1f9245145
thibaultJP <56250582+thibaultJP@users.noreply.github.com>
2020-05-03 13:10:42 +0200

    deplacement menu dans app.vue
    ajout d'un élément navbar pour gérer le menu

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,78 @@
 <template>
   <div id="app">
     <header>
-      <h1>
-        <router-link to="/master">My Daily Coach</router-link>
-      </h1>
+      <template v-if="user.loggedIn">
+       <b-container >
+          <b-row align-h="center">
+            <b-col lg="11">
+             <b-navbar toggleable="lg" type="light" >
+                <b-navbar-brand ><H1>My Daily Coach</H1> </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                  <!-- Right aligned nav items -->
+                  <b-navbar-nav class="ml-auto">   
+                    <b-button v-b-toggle.collapse-1 variant="white">Paramètre</b-button>               
+                    <b-button variant="danger" @click.prevent="signOut">Se déconnecter</b-button>
+                  </b-navbar-nav>
+                </b-collapse>
+              </b-navbar>
+            </b-col>
+          </b-row>
+        </b-container >
+      </template>
+      <template v-else>
+         <b-container >
+          <b-row align-h="start">
+            <b-col align-self="start">
+              <h1 class="mb-5">
+                My Daily Coach
+              </h1>
+            </b-col>
+          </b-row>
+        </b-container >
+      </template>
     </header>
+
+    <b-container>
+      <b-row>        
+        <b-collapse id="collapse-1" class="mt-2 col-12">
+          <b-card class="row text-center">
+            <b-col class="mb-4">
+              <p class="card-text">Quel horraire de recommandation voulez vous ?</p>
+              <b-button v-b-toggle.collapse-1-inner size="sm">Modifiez</b-button>
+              <b-collapse id="collapse-1-inner" class="mt-2">
+                <b-card>
+                  <div class="col-md-6">
+                    <div class="md-form md-outline input-with-post-icon timepicker" darktheme="true">
+                      <input type="text" id="dark-version-example" class="form-control" placeholder="Select time">
+                      <label for="dark-version-example">Dark version, 24 hours</label>
+                      <i class="fas fa-envelope  input-prefix"></i>
+                    </div>
+                  </div>
+                </b-card>
+              </b-collapse>
+            </b-col>
+            <b-col  class="mb-4">
+              <p class="card-text">Quel capteur voulez vous ?</p>
+              <b-button v-b-toggle.collapse-2-inner size="sm">Modifiez</b-button>
+              <b-collapse id="collapse-2-inner" class="mt-2">
+                <b-card>Hello!</b-card>
+              </b-collapse>
+            </b-col>
+            <b-col  class="mb-4">
+              <p class="card-text">Quel capteur voulez vous ?</p>
+              <b-button v-b-toggle.collapse-3-inner size="sm">Modifiez</b-button>
+              <b-collapse id="collapse-3-inner" class="mt-2">
+                <b-card>Hello!</b-card>
+              </b-collapse>
+            </b-col>
+          </b-card>
+        </b-collapse>
+      </b-row>
+    </b-container>
+
     <transition name="fade" mode="out-in">
       <router-view class="view"></router-view>
     </transition>
@@ -15,8 +83,29 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
+import * as firebase from "firebase";
 export default {
-  name: "App"
+  name: "App",
+   methods: {
+    signOut() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace({
+            name: "signin"
+          });
+        });
+    },
+    
+  },
+  computed: {
+    ...mapGetters({
+      user: "user"
+    })
+  }
 };
 </script>
 
@@ -42,7 +131,7 @@ header {
   font-size: 2em;
   color: #41b883;
   width: 100%;
-  text-align: center;
+  text-align: right;
   padding-top: 1em;
   padding-bottom: 1em;
 }
