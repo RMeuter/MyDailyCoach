72bc03233b983cb84ce5f43db246a4a59b660437
Romain Meuter <r.meuter@hotmail.com>
2020-05-05 21:29:53 +0200

    Séparation du template stat et profil user

---
+++ src/components/Profil.vue
@@ -0,0 +1,41 @@
+<template>
+    <b-col  cols="12">
+        <b-col cols="6">
+        <b-img :src="get_lvlImg()" fluid alt="Fluid image"></b-img> <br>
+        </b-col>
+        <b-col cols="6">
+        Votre nombre de point : {{niveau.nombreDePoint}} --- Votre progression : {{user.data.pointBienEtre}}
+        <div class="progress">
+            <div 
+            class="progress-bar progress-bar-striped progress-bar-animated" 
+            role="progressbar" 
+            :aria-valuenow="user.data.pointBienEtre" 
+            :aria-valuemin="niveau.nombreDePoint" 
+            :aria-valuemax="niveau.niveauSup" :style="progress">
+            </div>
+        </div>
+        {{niveau.commentaire}} <br>
+        </b-col>
+    </b-col>
+</template>
+
+<script>
+
+export default {
+  name: "StatsUser",
+  props:{
+      niveau:{},
+      user:{}
+  },
+  methods:{
+     get_lvlImg(){
+      return require(`../assets/LevelImage/${this.niveau.photoLevel}`)
+    },
+    progress(){
+      return {width :Math.round(100*((this.user.data.pointBienEtre-this.niveau.nombreDePoint)/this.niveau.niveauSup))}
+    }
+  },
+  
+};
+
+</script>
--- src/components/StatsUser.vue
+++ src/components/StatsUser.vue
@@ -1,25 +1,29 @@
 <template>
   <div>
-    <b-container > 
-      <!-- ############################# Presentation de l'application en trois points ############################# !-->     
+    <b-container v-if="user.data && niveau"> 
       <b-row>
-        <b-col>
+        <b-col cols="6">
           <h4> Salut {{user.data.displayName}} </h4>
         </b-col>
+        <!-- ############################# Infos gamifier ############################# !-->     
+        <Profil :user="user" :niveau="niveau" />
       </b-row>
-      <b-row v-if="firstTime"> 
-        <b-alert show variant="light" dismissible>
-          <p>Voici comment fonctionne l'application :</p>
-          <b-alert show variant="primary">
-            Vous devez installer Google Fit pour nous permettre de recupérer votre activité !
-            </b-alert>
-          <b-alert show variant="secondary">
-            Vous choisissez l'heure de votre Daily Activity !
-            </b-alert>
-          <b-alert show variant="success">
-            Profitez pleinnement de notre application ! 
-            </b-alert>
-        </b-alert>
+      <!-- ############################# Presentation de l'application en trois points ############################# !-->     
+      <b-row v-if="!user.data.parametre.NePlusVoirExplication">
+        <b-col cols="12">
+          <b-alert show variant="light" dismissible>
+            <p>Voici comment fonctionne l'application :</p>
+            <b-alert show variant="primary">
+              Vous devez installer Google Fit pour nous permettre de recupérer votre activité !
+              </b-alert>
+            <b-alert show variant="secondary">
+              Vous choisissez l'heure de votre Daily Activity !
+              </b-alert>
+            <b-alert show variant="success">
+              Profitez pleinnement de notre application ! 
+              </b-alert>
+          </b-alert>
+        </b-col> 
       </b-row>
       <b-row>
         <!-- ############################# Graphique de l'utilisateur ############################# !-->     
@@ -30,38 +34,54 @@
           <Mixed />
         </b-col>
       </b-row>          
-    </b-container>
-    <div v-if="montreBoutonRecommandation">
-        <b-link href="/signin">Accès à la recommandation de la journée</b-link>
+    <div v-if="this.$store.getters.dateRecommandation">
+        <router-link
+          :to="{name:'detail', params:{activite:activite, estRecommande:true, nomActivite:activite.nom}}"
+        >Lien vers l'activité !</router-link>
     </div>
+    </b-container>
+    <b-container v-else>
+      <div style="width:100%;height:0;padding-bottom:57%;position:relative;">
+        <iframe 
+        src="https://giphy.com/embed/3y0oCOkdKKRi0" 
+        width="100%" height="100%" style="position:absolute" 
+        frameBorder="0" class="giphy-embed" allowFullScreen
+        >
+        </iframe>
+      </div>
+    </b-container>
   </div>
 </template>
 
 <script>
 import Chart from "./Charts/Chart";
 import Mixed from "./Charts/Mixed";
+import Profil from "./Profil";
 import { mapGetters } from 'vuex';
+
 
 
 export default {
   name: "StatsUser",
   components:{
     Chart,
-    Mixed
+    Mixed,
+    Profil
   },
   methods:{
-    firstTime(){ // ça doit passer en props !
-      return true
+     get_lvlImg(){
+      return require(`../assets/LevelImage/${this.niveau.photoLevel}`)
+    },
+    progress(){
+      return {width :Math.round(100*((this.user.data.pointBienEtre-this.niveau.nombreDePoint)/this.niveau.niveauSup))}
     }
   },
   computed: {
-      montreBoutonRecommandation(){
-        return this.$store.getters.dateRecommandation;
-    },
     ...mapGetters({
-      user: "user"
-    })
-
+      user: "user",
+      niveau : "niveauObtenu"
+    }),
+   
   },
   
 };
