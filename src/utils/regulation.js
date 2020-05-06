eb593652560da2ba4c139e5a247d356d4592cbc1
Romain Meuter <r.meuter@hotmail.com>
2020-05-06 23:08:46 +0200

    Mise a jour des calcule type attribuer au données par capteur

--- src/utils/regulation.js
+++ src/utils/regulation.js
@@ -1,5 +1,6 @@
-export default {
+module.exports = {
     // ######### Entre Regulation (afin déviter le code spagetti et faire une code flexible sur les données):
+
     CapteurPas:{
         // Les pas sont issues de com.google.step_count.delta
         // La forme des données des pas nbPasEnUneHeure = [12, 23, ...] 
@@ -7,6 +8,7 @@ export default {
         Calcul(donneePas){
             return donneePas.reduce((accumulator, currentValue) => accumulator + currentValue)
         },
+        typeCapteur :1, // type 1 revèle plus de l'activité
         AugmentatioNFoixPlusEleve:
         /**Recommandation :
          * - Nécéssite que la liste soit trier dans l'ordre décroissant de la valeur N
@@ -30,36 +32,38 @@ export default {
             let arraySommeil = [72, 109, 110, 111, 112];
             for (let arrayActivite in donneeSommeil){
                 let i = 0;
-                while(i < arrayActivite.length){
-                    if(arraySommeil.includes(donneeSommeil[i])){
-                        tempsSommeil += donneeSommeil[i+1];
+                while(i < donneeSommeil[arrayActivite].length){
+                    if(arraySommeil.includes(donneeSommeil[arrayActivite][i])){
+                        tempsSommeil += donneeSommeil[arrayActivite][i+1];
                     }
                     i+=3
                 }
             }
             return tempsSommeil;
         },
+        typeCapteur :0, // type 0 revèle plus de l'anxiété
         AugmentatioNFoixPlusEleve:
             [ 
-                { N : 2, ajoutPoint : 3},
+                { N : 6/5, ajoutPoint : 3},
                 { N : 1, ajoutPoint : 2},
-                { N : 1/2, ajoutPoint : 0},
-                { N : 1/3, ajoutPoint : 1},
-                { N : 1/4, ajoutPoint : 2},
+                { N : 4/5, ajoutPoint : 0},
+                { N : 1/2, ajoutPoint : 1},
+                { N : 1/3, ajoutPoint : 2},
                 { N : 0, ajoutPoint : 3},
             ]
     },
-    PointCoeur:{
+    CapteurPointCoeur:{
         // Les pointsCoeurs est issue de com.google.heart_minutes
         // Le format des données est [nbPointCoeur, TempsPourObtenirDurantHeure] 
         // une array par heure (x24)
         Calcul(donneePointCoeur){
             let pointCumule = 0;
             for(let trancheHeure in donneePointCoeur){
-                pointCumule+=trancheHeure[0];
+                pointCumule+=donneePointCoeur[trancheHeure][0];
             }
             return pointCumule;
         },
+        typeCapteur :1, // type 1 revèle plus de l'activité
         AugmentatioNFoixPlusEleve:
             [ 
                 { N : 2, ajoutPoint : 3},
@@ -70,7 +74,7 @@ export default {
                 { N : 0, ajoutPoint : 3},
             ]
     },
-    CapteurFreqCardiaque:{
+    CapteurFrequencesCardiaque:{
         // Les FreqCardiaques sont issues de com.google.heart_rate.bpm
         // Le formats des données est [67, 68, 69, ...] 
         // une array par heure (x24)
@@ -78,13 +82,14 @@ export default {
             let pointCumule = 0;
             let nbCumule = 0;
             for (let freq in donneeFreqCardiaque){
-                for(let trancheHeure in freq){
-                    pointCumule+=trancheHeure[0];
+                for(let trancheHeure in donneeFreqCardiaque[freq]){
+                    pointCumule+=donneeFreqCardiaque[freq][trancheHeure];
                     nbCumule++
                 }
             }
-            return pointCumule/nbCumule;
+            return Math.round(pointCumule/nbCumule);
         },
+        typeCapteur :0, // type 0 revèle plus de l'anxiété
         AugmentatioNFoixPlusEleve:
             [ 
                 { N : 2, ajoutPoint : 3},
